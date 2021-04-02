ec3caa078e32da74b61f83a145bb47b229d4223c
dieonly <yusufsetya77@gmail.com>
2021-04-02 09:32:43 +0800

    Buat Input String Kedalam Variable

--- InputOutput.java
+++ InputOutput.java
@@ -4,5 +4,8 @@ public class InputOutput{
 	public static void main(String[] args){
 		Scanner Scanner = new Scanner(System.in);
 
+		String nama;
+		System.out.print("Masukkan nama anda: ");
+		nama = Scanner.nextLine();
 	}
 }
